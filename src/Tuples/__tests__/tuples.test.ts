8f7e98e5c5eceefaf97d493e8e1816ca1c4f02b1
tochibedford <musicofbyte@gmail.com>
2022-10-20 19:02:04 +0100

    vector cross product

--- src/Tuples/__tests__/tuples.test.ts
+++ src/Tuples/__tests__/tuples.test.ts
@@ -1,10 +1,10 @@
 import { Point, tupleSum, tupleSubtract, tuple, Vector, negateTuple, scalarMult, scalarDiv, magnitude, normalize } from '../index'
 
-describe("Creating Vectors and points from Tuples", ()=>{
+describe("Creating Vectors and points from Tuples", () => {
     let pointFromTuple = tuple(4.3, -4.2, 3.1, 1)
     let vectorFromTuple = tuple(4.3, -4.2, 3.1, 0)
 
-    test('A tuple is a Point when w is 1', ()=>{
+    test('A tuple is a Point when w is 1', () => {
         expect(pointFromTuple.x).toEqual(4.3)
         expect(pointFromTuple.y).toEqual(-4.2)
         expect(pointFromTuple.z).toEqual(3.1)
@@ -13,7 +13,7 @@ describe("Creating Vectors and points from Tuples", ()=>{
         expect(pointFromTuple).not.toBeInstanceOf(Vector)
     })
 
-    test('A tuple is a Vector when w is 0', ()=>{
+    test('A tuple is a Vector when w is 0', () => {
         expect(vectorFromTuple.x).toEqual(4.3)
         expect(vectorFromTuple.y).toEqual(-4.2)
         expect(vectorFromTuple.z).toEqual(3.1)
@@ -23,93 +23,90 @@ describe("Creating Vectors and points from Tuples", ()=>{
     })
 })
 
-describe("Creating Points", ()=>{
+describe("Creating Points", () => {
     let point = new Point(4, -4, 3)
     let pointFromTuple = tuple(4, -4, 3, 1)
-    test("Point class creates tuples with w = 1", ()=>{
+    test("Point class creates tuples with w = 1", () => {
         expect(point.equals(pointFromTuple as Point)).toBe(true)
     })
 })
 
-describe("Creating Vectors", ()=>{
+describe("Creating Vectors", () => {
     let vector = new Vector(4, -4, 3)
     let vectorFromTuple = tuple(4, -4, 3, 0)
-    test("Vector class creates tuples with w = 1", ()=>{
+    test("Vector class creates tuples with w = 1", () => {
         expect(vector.equals(vectorFromTuple as Vector)).toBe(true)
     })
 })
 
-describe("Operations on tuples", ()=>{
-    test("Adding 2 tuples: Point + Vector", ()=>{
+describe("Operations on tuples", () => {
+    test("Adding 2 tuples: Point + Vector", () => {
         const point = tuple(3, -2, 5, 1)
         const vector = tuple(-2, 3, 1, 0)
         const total = new Point(1, 1, 6)
         expect(tupleSum(point, vector).equals(total)).toBe(true)
     })
 
-    test("Adding two Points throws Error", ()=>{
+    test("Adding two Points throws Error", () => {
         const point1 = tuple(3, -2, 5, 1)
         const point2 = tuple(4, -2, 5, 1)
-        expect(()=>{tupleSum(point1, point2)}).toThrowError(RangeError)
+        expect(() => { tupleSum(point1, point2) }).toThrowError(RangeError)
     })
 
-    test("Subtracting 2 tuples: Point - Point -> Vector", ()=>{
-        const point1 = new Point(3,2,1)
-        const point2 = new Point(5,6,7)
-        const total = new Vector(-2, -4, -6)
-        expect(tupleSubtract(point1, point2).equals(total)).toBe(true)
+    test("Subtracting 2 tuples: Point - Point -> Vector", () => {
+        const point1 = new Point(3, 2, 1)
+        const point2 = new Point(5, 6, 7)
+        expect(tupleSubtract(point1, point2).components()).toEqual([-2, -4, -6, 0])
         expect(tupleSubtract(point1, point2)).toBeInstanceOf(Vector)
     })
 
-    test("Subtracting 2 tuples: Point - Vector -> Point", ()=>{
-        const point = new Point(3,2,1)
-        const vector = new Vector(5,6,7)
-        const total = new Point(-2, -4, -6)
-        expect(tupleSubtract(point, vector).equals(total)).toBe(true)
+    test("Subtracting 2 tuples: Point - Vector -> Point", () => {
+        const point = new Point(3, 2, 1)
+        const vector = new Vector(5, 6, 7)
+        expect(tupleSubtract(point, vector).components()).toEqual([-2, -4, -6, 1])
         expect(tupleSubtract(point, vector)).toBeInstanceOf(Point)
     })
 
-    test("Subtracting 2 vectors: Vector - Vector -> Vector", ()=>{
+    test("Subtracting 2 vectors: Vector - Vector -> Vector", () => {
         const vector1 = new Vector(3, 2, 1)
         const vector2 = new Vector(5, 6, 7)
-        const total = new Vector(-2, -4, -6)
-        expect(tupleSubtract(vector1, vector2).equals(total)).toBe(true)
+        expect(tupleSubtract(vector1, vector2).components()).toEqual([-2, -4, -6, 0])
     })
 
-    test("Subtracting Point from vector throws error", ()=>{
+    test("Subtracting Point from vector throws error", () => {
         const vector = new Vector(3, -2, 5)
         const point = new Point(4, -2, 5)
-        expect(()=>{tupleSubtract(vector, point)}).toThrowError(RangeError)
+        expect(() => { tupleSubtract(vector, point) }).toThrowError(RangeError)
     })
 
-    test("Negating a tuple", ()=>{
+    test("Negating a tuple", () => {
         const vector = tuple(1, -2, 3, 0) as Vector
         vector.negate()
-        expect(vector.equals(new Vector(-1, 2, -3))).toBe(true)
-        expect(negateTuple(vector).equals(new Vector(1, -2, 3))).toBe(true)
+        expect(vector.components()).toEqual([-1, 2, -3, 0])
+        expect(negateTuple(vector).components()).toEqual([1, -2, 3, 0])
     })
 
-    test("Multiplying a tuple by a scalar", ()=>{
+    test("Multiplying a tuple by a scalar", () => {
         const vector = tuple(1, -2, 3, 0) as Vector
-        expect(scalarMult(vector, 3.5).equals(new Vector(3.5, -7, 10.5))).toBe(true)
+        expect(scalarMult(vector, 3.5).components()).toEqual([3.5, -7, 10.5, 0])
     })
 
-    test("Multiplying a tuple by a scalar (fraction)", ()=>{
+    test("Multiplying a tuple by a scalar (fraction)", () => {
         const vector = tuple(1, -2, 3, 0) as Vector
-        expect(scalarMult(vector, 0.5).equals(new Vector(0.5, -1, 1.5))).toBe(true)
+        expect(scalarMult(vector, 0.5).components()).toEqual([0.5, -1, 1.5, 0])
     })
 
-    test("Dividing a tuple by a scalar", ()=>{
+    test("Dividing a tuple by a scalar", () => {
         const vector = tuple(1, -2, 3, 0) as Vector
-        expect(scalarDiv(vector, 2).equals(new Vector(0.5, -1, 1.5))).toBe(true)
+        expect(scalarDiv(vector, 2).components()).toEqual([0.5, -1, 1.5, 0])
     })
 
-    test("Magnitude of a tuple", ()=>{
-        const vector1 = new Vector(1,0,0)
-        const vector2 = new Vector(0,1,0)
-        const vector3 = new Vector(0,0,1)
-        const vector4 = new Vector(1,2,3)
-        const vector5 = new Vector(-1,-2,-3)
+    test("Magnitude of a tuple", () => {
+        const vector1 = new Vector(1, 0, 0)
+        const vector2 = new Vector(0, 1, 0)
+        const vector3 = new Vector(0, 0, 1)
+        const vector4 = new Vector(1, 2, 3)
+        const vector5 = new Vector(-1, -2, -3)
 
         expect(magnitude(vector1)).toEqual(1)
         expect(magnitude(vector2)).toEqual(1)
@@ -118,25 +115,33 @@ describe("Operations on tuples", ()=>{
         expect(magnitude(vector5)).toEqual(Math.sqrt(14))
     })
 
-    test("Normalizing vectors", ()=>{
+    test("Normalizing vectors", () => {
         const vector1 = new Vector(4, 0, 0)
         const vector2 = new Vector(1, 2, 3)
 
-        expect(normalize(vector1).equals(new Vector(1, 0, 0))).toBe(true)
-        expect(normalize(vector2).equals(new Vector(1/Math.sqrt(14), 2/Math.sqrt(14), 3/Math.sqrt(14)))).toBe(true)
+        expect(normalize(vector1).components()).toEqual([1, 0, 0, 0])
+        expect(normalize(vector2).components()).toEqual([1 / Math.sqrt(14), 2 / Math.sqrt(14), 3 / Math.sqrt(14), 0])
     })
 
-    test("Magnitude of a Normalized vector should always be 1", ()=>{
+    test("Magnitude of a Normalized vector should always be 1", () => {
         const vector = new Vector(1, 2, 3)
 
         expect(magnitude(normalize(vector))).toEqual(1)
     })
 
-    test("Vector dot product", ()=>{
+    test("Vector dot product", () => {
         const vector1 = new Vector(1, 2, 3)
         const vector2 = new Vector(2, 3, 4)
 
         expect(vector1.dot(vector2)).toEqual(20)
+    })
+
+    test("Vector cross product", () => {
+        const vector1 = new Vector(1, 2, 3)
+        const vector2 = new Vector(2, 3, 4)
+
+        expect(vector1.cross(vector2).components()).toEqual([-1, 2, -1, 0])
+        expect(vector2.cross(vector1).components()).toEqual([1, -2, 1, 0])
     })
 
 })
--- src/Tuples/index.ts
+++ src/Tuples/index.ts
@@ -3,8 +3,8 @@ type Ttuple = {
     y: number,
     z: number,
     w: 0 | 1,
-    components: ()=>number[],
-    equals: (other: Ttuple)=>boolean
+    components: () => number[],
+    equals: (other: Ttuple) => boolean
 }
 
 class Point implements Ttuple {
@@ -12,7 +12,7 @@ class Point implements Ttuple {
     y: number
     z: number
     w: 1
-    
+
     constructor(x: number, y: number, z: number) {
         this.x = x
         this.y = y
@@ -20,14 +20,14 @@ class Point implements Ttuple {
         this.w = 1
     }
 
-    components = ()=>{
+    components = () => {
         return [this.x, this.y, this.z, this.w]
     }
 
-    equals = (other: Ttuple)=>{
-        if (this.x === other.x && this.y === other.y && this.z === other.z && other instanceof Point){
+    equals = (other: Ttuple) => {
+        if (this.x === other.x && this.y === other.y && this.z === other.z && other instanceof Point) {
             return true
-        }else{
+        } else {
             return false
         }
     }
@@ -46,79 +46,83 @@ class Vector implements Ttuple {
         this.w = 0
     }
 
-    components = ()=>{
+    components = () => {
         return [this.x, this.y, this.z, this.w]
+    }
+
+    cross = (other: Ttuple) => {
+        return new Vector((this.y*other.z - this.z * other.y), -(this.x*other.z-this.z*other.x), (this.x*other.y-this.y*other.x))
     }
 
     dot = (other: Ttuple): number => {
         return this.x * other.x + this.y * other.y + this.z * other.z + this.w * other.w
     }
 
-    equals = (other: Ttuple)=>{
-        if (this.x === other.x && this.y === other.y && this.z === other.z && other instanceof Vector){
+    equals = (other: Ttuple) => {
+        if (this.x === other.x && this.y === other.y && this.z === other.z && other instanceof Vector) {
             return true
-        }else{
+        } else {
             return false
         }
     }
 
-    negate = ()=>{
+    negate = () => {
         this.x = -this.x
         this.y = -this.y
         this.z = -this.z
     }
 }
 
-function tuple (x: number, y: number, z: number, w: 0|1 ): Ttuple {
+function tuple(x: number, y: number, z: number, w: 0 | 1): Ttuple {
     if (w === 1) {
         return new Point(x, y, z)
     } else {
-        return new  Vector(x, y, z)
+        return new Vector(x, y, z)
     }
 }
 
-function tupleSum (tuple1: Ttuple, tuple2: Ttuple): Ttuple {
-    if(tuple1 instanceof Point && tuple2 instanceof Point){
+function tupleSum(tuple1: Ttuple, tuple2: Ttuple): Ttuple {
+    if (tuple1 instanceof Point && tuple2 instanceof Point) {
         throw RangeError("Cannot add 2 Points; w becomes > 1")
     }
     return tuple(tuple1.x + tuple2.x, tuple1.y + tuple2.y, tuple1.z + tuple2.z, Math.abs(tuple1.w + tuple2.w) as 0 | 1)
 }
 
-function tupleSubtract (tuple1: Ttuple, tuple2: Ttuple): Ttuple {
-    if(tuple1 instanceof Vector && tuple2 instanceof Point){
+function tupleSubtract(tuple1: Ttuple, tuple2: Ttuple): Ttuple {
+    if (tuple1 instanceof Vector && tuple2 instanceof Point) {
         throw RangeError("Cannot subtract Point from Vector; w becomes -1")
     }
-    return tuple(tuple1.x - tuple2.x, tuple1.y - tuple2.y, tuple1.z - tuple2.z, Math.abs(tuple1.w-tuple2.w) as 0 | 1)
+    return tuple(tuple1.x - tuple2.x, tuple1.y - tuple2.y, tuple1.z - tuple2.z, Math.abs(tuple1.w - tuple2.w) as 0 | 1)
 }
 
-function negateTuple (tuple1: Ttuple): Ttuple {
+function negateTuple(tuple1: Ttuple): Ttuple {
     return tuple(-tuple1.x, -tuple1.y, -tuple1.z, tuple1.w)
 }
 
-function scalarMult (tuple1: Ttuple, factor: number) {
+function scalarMult(tuple1: Ttuple, factor: number) {
     return tuple(tuple1.x * factor, tuple1.y * factor, tuple1.z * factor, tuple1.w)
 }
 
-function scalarDiv (tuple1: Ttuple, factor: number) {
+function scalarDiv(tuple1: Ttuple, factor: number) {
     return tuple(tuple1.x / factor, tuple1.y / factor, tuple1.z / factor, tuple1.w)
 }
 
 function magnitude(tuple: Ttuple): number {
-    return Math.sqrt(tuple.components().reduce((prev, curr)=>{
-        prev += curr**2
+    return Math.sqrt(tuple.components().reduce((prev, curr) => {
+        prev += curr ** 2
         return prev
     }, 0))
 }
 
 function normalize(tuple: Ttuple): Ttuple {
     const tupleMag = magnitude(tuple)
-    const [nX, nY, nZ] = tuple.components().map(component=>{
-        return component/tupleMag
+    const [nX, nY, nZ] = tuple.components().map(component => {
+        return component / tupleMag
     })
     return new Vector(nX, nY, nZ)
 }
 
-export { 
+export {
     tuple,
     tupleSum,
     tupleSubtract,

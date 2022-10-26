1e607cc52ddeeb33e7f9e4000c353538659d8dc7
tochibedford <musicofbyte@gmail.com>
2022-10-26 01:09:27 +0100

    ⚙ build: recompiled documented code

--- dist/Tuples/index.js
+++ dist/Tuples/index.js
@@ -11,7 +11,7 @@ class Point {
     constructor(x, y, z) {
         /**
          * This method gives the current state of the point instance
-         * @remarks This method returns 4 numbers, the first 3 representing the current position of the x,y,z coordinate system and a 4th number 'w' representing the fact that it is a Point and not a {@link Vector}
+         * @remarks This method returns 4 numbers, the first 3 representing the current position in the x,y,z coordinate system and a 4th number 'w' representing the fact that it is a Point and not a {@link Vector}
          * @returns An array containing of numbers representing the current state/position of the Point instance
          */
         this.components = () => {
@@ -29,6 +29,14 @@ class Point {
                 return false;
             }
         };
+        /**
+         * Negates the values of the components of the Point, except for component w
+         */
+        this.negate = () => {
+            this.x = -this.x;
+            this.y = -this.y;
+            this.z = -this.z;
+        };
         this.x = x;
         this.y = y;
         this.z = z;
@@ -40,15 +48,34 @@ class Point {
  */
 class Vector {
     constructor(x, y, z) {
+        /**
+         * This method gives the current state of the vector instance
+         * @remarks This method returns 4 numbers, the first 3 representing the current values of the x,y,z directions and a 4th number 'w' representing the fact that it is a Vector and not a {@link Point}
+         * @returns An array containing of numbers representing the current state of the Vector instance
+         */
         this.components = () => {
             return [this.x, this.y, this.z, this.w];
         };
+        /**
+         * Performs a cross multiplication of two tuples, mostly vectors
+         * @param other The second tuple to cross multiply with
+         * @returns A new vector that is the cross product of the other two vectors
+         */
         this.cross = (other) => {
             return new Vector((this.y * other.z - this.z * other.y), -(this.x * other.z - this.z * other.x), (this.x * other.y - this.y * other.x));
         };
+        /**
+         * Performs a dot product operation on two tuples.
+         * @param other The second tuple to peform a dot product with
+         * @returns A number that is the result of the dot product of the two tuples
+         */
         this.dot = (other) => {
             return this.x * other.x + this.y * other.y + this.z * other.z + this.w * other.w;
         };
+        /**
+         * Checks if the current vector instance is identical to some other given vector instance and that the other vector is an instance of {@link Vector}
+         * @param other - Another tuple (Vector) to check for equality
+         */
         this.equals = (other) => {
             if (this.x === other.x && this.y === other.y && this.z === other.z && other instanceof Vector) {
                 return true;
@@ -57,6 +84,9 @@ class Vector {
                 return false;
             }
         };
+        /**
+         * Negates the values of the components of the Vector, except for component w
+         */
         this.negate = () => {
             this.x = -this.x;
             this.y = -this.y;
@@ -68,6 +98,15 @@ class Vector {
         this.w = 0;
     }
 }
+/**
+ * A factory function that creates either a tuple that is either a {@link Vector} or a {@link Point}
+ * if w is equal to 1 it returns a point else it will return a vector
+ * @param x Component x
+ * @param y Component y
+ * @param z Component z
+ * @param w Component w
+ * @returns A tuple that is either a Vector or a Point
+ */
 function tuple(x, y, z, w) {
     if (w === 1) {
         return new Point(x, y, z);
@@ -76,33 +115,121 @@ function tuple(x, y, z, w) {
         return new Vector(x, y, z);
     }
 }
+/**
+ * Produces the sum of two tuples, the 2 tuples can be of any kind but both cannot be a {@link Point} at the same time
+ * Point + Vector -> Point
+ * Vector + Vector -> Vector
+ * Point + Point -/- invalid
+ *
+ * @example
+ * ```
+ * const point = tuple(3, -2, 5, 1)
+ * const vector = tuple(-2, 3, 1, 0)
+ * tupleSum(point, vector).components() //->[1,1,6,1]
+ * ```
+ * @param tuple1 Tuple to sum
+ * @param tuple2 Tuple to sum
+ * @returns A new tuple that
+ * @throws RangeError - Thrown if user attempts to add two Points
+ */
 function tupleSum(tuple1, tuple2) {
     if (tuple1 instanceof Point && tuple2 instanceof Point) {
         throw RangeError("Cannot add 2 Points; w becomes > 1");
     }
     return tuple(tuple1.x + tuple2.x, tuple1.y + tuple2.y, tuple1.z + tuple2.z, Math.abs(tuple1.w + tuple2.w));
 }
+/**
+ * Produces the sum of two tuples, the 2 tuples can be of any kind but a {@link Point} cannot be subtracted from a {@link Vector}
+ * Point - Vector -> Point
+ * Vector - Vector -> Vector
+ * Point - Point -> Vector
+ * Vector - Point -/- invalid
+ * @example
+ * ```
+ * const point1 = new Point(3, 2, 1)
+ * const point2 = new Point(5, 6, 7)
+ * tupleSubtract(point1, point2).components() //-> [-2, -4, -6, 0]
+ * ```
+ * @param tuple1 Tuple to subtract from
+ * @param tuple2 Tuple to subtract
+ * @returns A tuple that is the result of the differece between tuple1 & tuple2 (tuple1-tuple2)
+ *
+ * @throws RangeError - Thrown if user is attempting to subtract a Point from a Vector i.e (Vector-Point)
+ */
 function tupleSubtract(tuple1, tuple2) {
     if (tuple1 instanceof Vector && tuple2 instanceof Point) {
         throw RangeError("Cannot subtract Point from Vector; w becomes -1");
     }
     return tuple(tuple1.x - tuple2.x, tuple1.y - tuple2.y, tuple1.z - tuple2.z, Math.abs(tuple1.w - tuple2.w));
 }
+/**
+ * Negates the values of the components of a tuple, except for component w.
+ * This can be used where you do not want the negation to affect the original tuple but rather to produce a new, negated tuple
+ * @example
+ * ```
+ * const vector = tuple(1, -2, 3, 0) as Vector
+ * negateTuple(vector).components() // [-1, 2, -3, 0]
+ * ```
+ * @param tuple1 Tuple to negate
+ * @returns A tuple that is the result of negating the components (besides w) of a given tuple
+ */
 function negateTuple(tuple1) {
     return tuple(-tuple1.x, -tuple1.y, -tuple1.z, tuple1.w);
 }
+/**
+ * Performs a scalar multiplication on a tuple by a given factor.
+ * @example
+ * ```
+ * const vector = tuple(1, -2, 3, 0) as Vector
+ * scalarMult(vector, 3.5).components() //-> [3.5, -7, 10.5, 0]
+ * ```
+ * @param tuple1 Tuple to scale
+ * @param factor Factor by which to multiply the components of the tuple
+ * @returns A new tuple with its components scaled by a given factor
+ */
 function scalarMult(tuple1, factor) {
     return tuple(tuple1.x * factor, tuple1.y * factor, tuple1.z * factor, tuple1.w);
 }
+/**
+ * Performs a scalar division on a tuple by a given factor.
+ * @example
+ * ```
+ * const vector = tuple(1, -2, 3, 0) as Vector
+ * scalarDiv(vector, 2).components() //-> [0.5, -1, 1.5, 0]
+ * ```
+ * @param tuple1 Tuple to scale
+ * @param factor Factor by which to divide the components of the tuple
+ * @returns A new tuple with its components scaled by a given factor
+ */
 function scalarDiv(tuple1, factor) {
     return tuple(tuple1.x / factor, tuple1.y / factor, tuple1.z / factor, tuple1.w);
 }
+/**
+ * Produces the magnitude of the given tuple
+ * @example
+ * ```
+ * const vector = new Vector(1, 2, 3)
+ * magnitude(vector) //-> 3.7416573867739413
+ * ```
+ * @param tuple Tuple to find magnitude of
+ * @returns A number that represents the magnitude of the given tuple
+ */
 function magnitude(tuple) {
     return Math.sqrt(tuple.components().reduce((prev, curr) => {
         prev += Math.pow(curr, 2);
         return prev;
     }, 0));
 }
+/**
+ * Normalizes the components of a vector such that its magnitude is now 1.
+ * @example
+ * ```
+ * const vector = new Vector(4, 0, 0)
+ * normalize(vector1).components() //-> [1, 0, 0, 0]
+ * ```
+ * @param tuple Tuple to normalize
+ * @returns A new normalized Vector
+ */
 function normalize(tuple) {
     const tupleMag = magnitude(tuple);
     const [nX, nY, nZ] = tuple.components().map(component => {
--- dist/types/Tuples/index.d.ts
+++ dist/types/Tuples/index.d.ts
@@ -10,6 +10,7 @@ declare type Ttuple = {
     w?: 0 | 1;
     components: () => number[];
     equals: (other: Ttuple) => boolean;
+    negate: () => void;
 };
 /**
  * A class that implements the {@link Ttuple} base type and represents a literal point a a 3-Dimensional Coordinate system
@@ -28,7 +29,7 @@ declare class Point implements Ttuple {
     constructor(x: number, y: number, z: number);
     /**
      * This method gives the current state of the point instance
-     * @remarks This method returns 4 numbers, the first 3 representing the current position of the x,y,z coordinate system and a 4th number 'w' representing the fact that it is a Point and not a {@link Vector}
+     * @remarks This method returns 4 numbers, the first 3 representing the current position in the x,y,z coordinate system and a 4th number 'w' representing the fact that it is a Point and not a {@link Vector}
      * @returns An array containing of numbers representing the current state/position of the Point instance
      */
     components: () => number[];
@@ -37,6 +38,10 @@ declare class Point implements Ttuple {
      * @param other
      */
     equals: (other: Ttuple) => boolean;
+    /**
+     * Negates the values of the components of the Point, except for component w
+     */
+    negate: () => void;
 }
 /**
  * A class that implements the {@link Ttuple} base type and represents a Vector a a 3-Dimensional Coordinate system
@@ -47,19 +52,138 @@ declare class Vector implements Ttuple {
     z: number;
     w: 0;
     constructor(x: number, y: number, z: number);
+    /**
+     * This method gives the current state of the vector instance
+     * @remarks This method returns 4 numbers, the first 3 representing the current values of the x,y,z directions and a 4th number 'w' representing the fact that it is a Vector and not a {@link Point}
+     * @returns An array containing of numbers representing the current state of the Vector instance
+     */
     components: () => number[];
+    /**
+     * Performs a cross multiplication of two tuples, mostly vectors
+     * @param other The second tuple to cross multiply with
+     * @returns A new vector that is the cross product of the other two vectors
+     */
     cross: (other: Ttuple) => Vector;
+    /**
+     * Performs a dot product operation on two tuples.
+     * @param other The second tuple to peform a dot product with
+     * @returns A number that is the result of the dot product of the two tuples
+     */
     dot: (other: Ttuple) => number;
+    /**
+     * Checks if the current vector instance is identical to some other given vector instance and that the other vector is an instance of {@link Vector}
+     * @param other - Another tuple (Vector) to check for equality
+     */
     equals: (other: Ttuple) => boolean;
+    /**
+     * Negates the values of the components of the Vector, except for component w
+     */
     negate: () => void;
 }
+/**
+ * A factory function that creates either a tuple that is either a {@link Vector} or a {@link Point}
+ * if w is equal to 1 it returns a point else it will return a vector
+ * @param x Component x
+ * @param y Component y
+ * @param z Component z
+ * @param w Component w
+ * @returns A tuple that is either a Vector or a Point
+ */
 declare function tuple(x: number, y: number, z: number, w: 0 | 1): Ttuple;
+/**
+ * Produces the sum of two tuples, the 2 tuples can be of any kind but both cannot be a {@link Point} at the same time
+ * Point + Vector -> Point
+ * Vector + Vector -> Vector
+ * Point + Point -/- invalid
+ *
+ * @example
+ * ```
+ * const point = tuple(3, -2, 5, 1)
+ * const vector = tuple(-2, 3, 1, 0)
+ * tupleSum(point, vector).components() //->[1,1,6,1]
+ * ```
+ * @param tuple1 Tuple to sum
+ * @param tuple2 Tuple to sum
+ * @returns A new tuple that
+ * @throws RangeError - Thrown if user attempts to add two Points
+ */
 declare function tupleSum(tuple1: Ttuple, tuple2: Ttuple): Ttuple;
+/**
+ * Produces the sum of two tuples, the 2 tuples can be of any kind but a {@link Point} cannot be subtracted from a {@link Vector}
+ * Point - Vector -> Point
+ * Vector - Vector -> Vector
+ * Point - Point -> Vector
+ * Vector - Point -/- invalid
+ * @example
+ * ```
+ * const point1 = new Point(3, 2, 1)
+ * const point2 = new Point(5, 6, 7)
+ * tupleSubtract(point1, point2).components() //-> [-2, -4, -6, 0]
+ * ```
+ * @param tuple1 Tuple to subtract from
+ * @param tuple2 Tuple to subtract
+ * @returns A tuple that is the result of the differece between tuple1 & tuple2 (tuple1-tuple2)
+ *
+ * @throws RangeError - Thrown if user is attempting to subtract a Point from a Vector i.e (Vector-Point)
+ */
 declare function tupleSubtract(tuple1: Ttuple, tuple2: Ttuple): Ttuple;
+/**
+ * Negates the values of the components of a tuple, except for component w.
+ * This can be used where you do not want the negation to affect the original tuple but rather to produce a new, negated tuple
+ * @example
+ * ```
+ * const vector = tuple(1, -2, 3, 0) as Vector
+ * negateTuple(vector).components() // [-1, 2, -3, 0]
+ * ```
+ * @param tuple1 Tuple to negate
+ * @returns A tuple that is the result of negating the components (besides w) of a given tuple
+ */
 declare function negateTuple(tuple1: Ttuple): Ttuple;
+/**
+ * Performs a scalar multiplication on a tuple by a given factor.
+ * @example
+ * ```
+ * const vector = tuple(1, -2, 3, 0) as Vector
+ * scalarMult(vector, 3.5).components() //-> [3.5, -7, 10.5, 0]
+ * ```
+ * @param tuple1 Tuple to scale
+ * @param factor Factor by which to multiply the components of the tuple
+ * @returns A new tuple with its components scaled by a given factor
+ */
 declare function scalarMult(tuple1: Ttuple, factor: number): Ttuple;
+/**
+ * Performs a scalar division on a tuple by a given factor.
+ * @example
+ * ```
+ * const vector = tuple(1, -2, 3, 0) as Vector
+ * scalarDiv(vector, 2).components() //-> [0.5, -1, 1.5, 0]
+ * ```
+ * @param tuple1 Tuple to scale
+ * @param factor Factor by which to divide the components of the tuple
+ * @returns A new tuple with its components scaled by a given factor
+ */
 declare function scalarDiv(tuple1: Ttuple, factor: number): Ttuple;
+/**
+ * Produces the magnitude of the given tuple
+ * @example
+ * ```
+ * const vector = new Vector(1, 2, 3)
+ * magnitude(vector) //-> 3.7416573867739413
+ * ```
+ * @param tuple Tuple to find magnitude of
+ * @returns A number that represents the magnitude of the given tuple
+ */
 declare function magnitude(tuple: Ttuple): number;
+/**
+ * Normalizes the components of a vector such that its magnitude is now 1.
+ * @example
+ * ```
+ * const vector = new Vector(4, 0, 0)
+ * normalize(vector1).components() //-> [1, 0, 0, 0]
+ * ```
+ * @param tuple Tuple to normalize
+ * @returns A new normalized Vector
+ */
 declare function normalize(tuple: Ttuple): Ttuple;
 export { Ttuple, tuple, tupleSum, tupleSubtract, negateTuple, scalarMult, scalarDiv, magnitude, normalize, Point, Vector, };
 //# sourceMappingURL=index.d.ts.map

52108a11e57d332a680befc0856c263308279ef5
tochibedford <musicofbyte@gmail.com>
2023-03-19 19:12:31 +0100

    implemented dummy unit sphere

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,7 +1,9 @@
-function fEqual (a: number, b: number, EPS: number = 0.0001) { // comparing 2 floating point numbers
-    if (Math.abs(a-b) < EPS){
+import { Point } from "./Tuples";
+
+function fEqual(a: number, b: number, EPS: number = 0.0001) { // comparing 2 floating point numbers
+    if (Math.abs(a - b) < EPS) {
         return true
-    }else {
+    } else {
         return false
     }
 }
@@ -11,18 +13,27 @@ function replaceAt(original: string, index: number, replacement: string) {
 }
 
 function dotProduct(array1: number[], array2: number[]) {
-    if(array1.length !== array2.length) {
+    if (array1.length !== array2.length) {
         throw Error("Both tuples/Arrays have to be the same length")
     }
     let sum = 0
-    array1.forEach((num, index)=>{
+    array1.forEach((num, index) => {
         sum += num * array2[index]
     })
     return sum
 }
 
+class Sphere {
+    position: Point
+    constructor() {
+        //for now assume every sphere is positioned at origin -> 0,0,0
+        this.position = new Point(0, 0, 0)
+    }
+}
+
 export {
     fEqual,
     replaceAt,
-    dotProduct
+    dotProduct,
+    Sphere
 }
